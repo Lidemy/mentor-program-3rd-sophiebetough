13dac2870d533af8d1dd84abccf5cf78a5d8cf91
CMCHANG <st89052511@gmail.com>
2019-05-06 20:36:51 +0900

    hw2.finished

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,14 @@
 function alphaSwap(str) {
-  console.log(str);
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] >= 'a' && str[i] <= 'z') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i].toLowerCase();
+    }
+  }
+  return result;
 }
+
 
 module.exports = alphaSwap;
