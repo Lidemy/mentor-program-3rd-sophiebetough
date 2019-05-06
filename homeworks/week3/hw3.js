16998422b2e17cee7a47ecbf10cd9e3e23bdc2c2
CMCHANG <st89052511@gmail.com>
2019-05-06 22:39:28 +0900

    hw3.finished

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,11 @@
 function isPrime(n) {
-  console.log(n);
+  if (n === 1) return false;
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
+
+console.log(isPrime(2));
 
 module.exports = isPrime;
