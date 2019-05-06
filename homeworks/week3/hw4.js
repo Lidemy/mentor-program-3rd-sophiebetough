cc70acb67957d4d4922098c98d618053bb7d02d0
CMCHANG <st89052511@gmail.com>
2019-05-07 00:00:26 +0900

    hw4.finished

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,13 @@
+function reverse(str) {
+  let reverseStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    reverseStr += str[i];
+  }
+  return reverseStr;
+}
+
 function isPalindromes(str) {
-  console.log(str);
+  return str === reverse(str);
 }
 
 module.exports = isPalindromes;
