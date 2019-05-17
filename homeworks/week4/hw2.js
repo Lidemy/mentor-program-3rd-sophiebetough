a836448156b9464a776102e498c92296e07c6bca
CMCHANG <st89052511@gmail.com>
2019-05-17 23:01:08 +0900

    hw3.finished

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -2,7 +2,7 @@ const request = require('request');
 const process = require('process');
 
 if (process.argv[2] === 'list') {
-  request('https://lidemy-book-store.herokuapp.com/books?_limit=20',
+  request.get('https://lidemy-book-store.herokuapp.com/books?_limit=20',
     (error, response, body) => {
       const book = JSON.parse(body);
       for (let i = 0; i < book.length; i += 1) {
@@ -10,7 +10,7 @@ if (process.argv[2] === 'list') {
       }
     });
 } else if (process.argv[2] === 'book') {
-  request(`https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
+  request.get(`https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
     (error, response, body) => {
       const book = JSON.parse(body);
       console.log(book.name);
